71fa5ebfee638ce6d9ab7f17fbd3cd9e652226c4
s <s.looi2@outlook.com>
2023-11-27 18:48:50 +1300

    Separate files

--- app.ts
+++ app.ts
@@ -1,10 +1,9 @@
 import express from "express"
 
-const PORT = 5000
 const app = express()
 
 app.get("/",(req,res)=>{
 	res.send("hi")
 })
 
-app.listen(PORT,()=>{console.log("LISTENING ON PORT "+PORT)})
+export {app}
---
+++ entry.ts
@@ -0,0 +1,5 @@
+
+import {app} from "./app"
+
+const PORT = 5000
+app.listen(PORT,()=>{console.log("LISTENING ON PORT "+PORT)})
